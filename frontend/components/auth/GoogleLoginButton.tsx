5f11e919cba452b623f7730b521518eab973d8dc
GioTaipe <120596675+GioTaip@users.noreply.github.com>
2026-03-16 17:46:20 +0100

    fix GOOGLE

--- frontend/components/auth/GoogleLoginButton.tsx
+++ frontend/components/auth/GoogleLoginButton.tsx
@@ -10,7 +10,8 @@ interface GoogleLoginButtonProps {
 const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "";
 //const REDIRECT_URI = typeof window !== "undefined" ? window.location.origin : "";
 const REDIRECT_URI = process.env.NEXT_PUBLIC_APP_URL_FRONTEND ?? (typeof window !== "undefined" ? window.location.origin : "");
-
+console.log("REDIRECT_URI:", REDIRECT_URI); // 👈
+console.log("APP_URL env:", process.env.NEXT_PUBLIC_APP_URL); 
 export default function GoogleLoginButton({ onSuccess, onError }: GoogleLoginButtonProps) {
   const callbackRef = useRef(onSuccess);
   const errorRef = useRef(onError);
